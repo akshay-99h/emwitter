ccbe4eed4f4822de1bb1b20a47f45fad31060c9d
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2024-07-29 13:41:18 +0530

    feat: scaffold sentry files

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://19a568cb5d82bb108ecbc53077f426ce@o4507683427516416.ingest.us.sentry.io/4507683439509504",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
